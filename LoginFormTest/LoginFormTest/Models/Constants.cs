86ae7b27a7c7cbd30a61c684e08de88e5d9ddb09
Stefano Magistri <smagistri@intelligentenvironments.com>
2018-05-14 15:33:33 +0100

    tees 3

---
+++ LoginFormTest/LoginFormTest/Models/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginFormTest
+{
+    public static class Constants
+    {
+        public const string LoginDbConnectionString =
+            @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\smagistri\source\repos\LoginFormTest\LoginFormTest\LoginFormTest\DB\loginDB.mdf; Integrated Security = True; Connect Timeout = 30; Initial Catalog=loginDB;";
+    }
+}
